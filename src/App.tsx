644259fba83a909ee181608d20aca25628aefab6
Jack <jack@shapeng1998.com>
2022-07-13 23:53:51 +0800

    chore: bump vite to 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main className="flex flex-col items-center">
       <p>{count}</p>
-      <button className="btn" onClick={() => setCount(c => c + 1)}>
+      <button className="btn" onClick={() => setCount((c) => c + 1)}>
         add
       </button>
     </main>
